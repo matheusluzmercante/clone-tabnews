ae90a30d30dc2d4ff05b194103fb349d88f3821d
Matheus Luz Mercante <61236661+matheusluzmercante@users.noreply.github.com>
2024-02-02 20:38:17 +0000

    adicionado os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,12 @@ function Home() {
   return <h1>Em construção...</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("identação errada");
+}
+
 export default Home;
