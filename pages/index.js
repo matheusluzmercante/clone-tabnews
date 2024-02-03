836c12abfa06c4b967d08379b67f635631cd9924
Matheus Luz Mercante <61236661+matheusluzmercante@users.noreply.github.com>
2024-02-03 18:54:55 +0000

    modificada a frase

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Em construção...</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-  console.log("identação errada");
-}
-
 export default Home;
